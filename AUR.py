8c6e25f7a3dc42d1c89d4c9b4fe0c15d00e2af50
Xylot <Joseph.Meriam@gmail.com>
2018-12-27 03:09:59 -0600

    Refactor
    Currently has a bug not allowing the script to read the list of wanted media from a file.

--- AUR.py
+++ AUR.py
@@ -4,31 +4,10 @@ import shutil
 import logging
 import time
 
-# AutoUnRAR.bat "The.Big.Bang.Theory.S11E05.720p.HDTV.X264-DIMENSION" "X:\\Completed Torrents\\The.Big.Bang.Theory.S11E05.720p.HDTV.X264-DIMENSION"
-
-# python AUR.py "South.Park.S22E10.HDTV.x264-SVA" "K:\Completed Torrents\South.Park.S22E10.HDTV.x264-SVA"
-
-
-def main():
-
-	destPath = 'K:\\Unsorted TV Shows'
-	mediaList = readFile()
-	arguments = sys.argv
-
-	print(mediaList)
-
-	torrentName = arguments[1]
-	torrentPath = ' '.join(str(elem) for elem in arguments[2:])
-
-	if isWanted(torrentName, mediaList):
-		args = ['C:\\Program Files\\WinRAR\\WinRAR.exe', 'x', '-ibck', '-inul', torrentPath + '\\*.r00' , destPath]
-		print(args)
-		subprocess.call(args)
-
-
-def printArguments():
-	print('Number of arguments:', len(sys.argv))
-	print('Argument List:', str(sys.argv))
+mediaList = ['flash', 'arrow', 'legends', 'blacklist', 
+				'bang', 'super', 'kai', 'walking', 
+				'supergirl', '100', 'south', 'world','sheldon', 
+				'robot', 'walking', 'silicon']
 
 def readFile():
 	names = []
@@ -36,25 +15,27 @@ def readFile():
 		for line in file:
 			line = line.rstrip('\n').lower()
 			names.append(line)
-
 	return names
 
-def writeArguments(args):
-	with open('arguments.txt', 'w') as file:
-		for arg in args:
-			file.write(arg + "\n")
-
-def sanitizeMediaList(mediaList):
-	for media in mediaList:
-		media = media[:-2].lower()
-	return mediaList
-
-def isWanted(fileName, mediaList):
+def isWanted(fileName):
 	for media in mediaList:
 		if media in fileName.lower():
 			return True
 	return False
-	# if any(name in fileName.lower() for name in mediaList):
-	# 	return True
+
+def main():
+
+	destPath = 'K:\\Unsorted TV Shows'
+	winrarPath = 'C:\\Program Files\\WinRAR\\WinRAR.exe'
+
+	arguments = sys.argv
+
+	torrentName = arguments[1]
+	torrentPath = ' '.join(str(elem) for elem in arguments[2:])
+	torrentPath = torrentPath.replace('\\', '/')
+
+	if isWanted(torrentName):
+		args = [winrarPath, 'x', '-ibck', '-inul', torrentPath + '\\*.r00' , destPath]
+		subprocess.call(args)
 
 main()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# AutoExtractor
+# Traga
 Automatically extracts torrents
